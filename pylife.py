957f47bff1075587709467bccbd7083c9055cc80
Alex <alexmoberhofer@gmail.com>
2020-05-06 18:08:36 -0400

    current default config

--- pylife.py
+++ pylife.py
@@ -2,14 +2,15 @@ import time
 import numpy as np
 import sys
 import ctypes
+import sdl2.ext
 from ctypes import *
 from sdl2 import *
 from copy import deepcopy
 
 PIX_ON = 0xFFFFFF
-PIX_OFF = 0
+PIX_OFF = 0x000000
 pixel_states = [PIX_ON, PIX_OFF]
-grid_size = 100
+grid_size = 200
 
 
 def randomGrid(N):
@@ -28,7 +29,7 @@ def update(grid, N):
             total = int((grid[i, (j-1) % N] + grid[i, (j+1) % N] +
                          grid[(i-1) % N, j] + grid[(i+1) % N, j] +
                          grid[(i-1) % N, (j-1) % N] + grid[(i-1) % N, (j+1) % N] +
-                         grid[(i+1) % N, (j-1) % N] + grid[(i+1) % N, (j+1) % N])/0xFFFFFF)
+                         grid[(i+1) % N, (j-1) % N] + grid[(i+1) % N, (j+1) % N])/PIX_ON)
             # apply Conway's rules
             if grid[i, j] == PIX_ON:
                 if (total < 2) or (total > 3):
@@ -46,9 +47,11 @@ def main():
 
     SDL_Init(SDL_INIT_VIDEO)
 
+    scale = 2
+
     window = SDL_CreateWindow(b"PyLife",
                               SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              grid_size * 4, grid_size * 4, SDL_WINDOW_SHOWN)
+                              grid_size * scale, grid_size * scale, SDL_WINDOW_SHOWN)
 
     windowsurface = SDL_GetWindowSurface(window)
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED)
@@ -61,19 +64,35 @@ def main():
     print("Board Initalized")
 
     iterations = 0
-    while 1:
+    running = True
+    while running:
+        events = sdl2.ext.get_events()
+
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
+        
+        #iterate board state
         update(game_board, grid_size)
+        iterations += 1
+
+        new_title = b"PyLife                      Iteration - " + str(iterations).encode('utf-8')
+        SDL_SetWindowTitle(window, new_title)
+
+        #convert to 2d to contiguous array
         c_array = np.ascontiguousarray(game_board, dtype=int)
+
+        #change to c type sdl expects
         pointer = c_array.ctypes.data_as(ctypes.c_void_p)
 
-        print("Texture converted")
+        #update our texture
         SDL_UpdateTexture(texture, None, pointer, grid_size * 4)
-        print("Texture updated")
         SDL_RenderClear(renderer)
         SDL_RenderCopy(renderer, texture, None, None)
         SDL_RenderPresent(renderer)
         SDL_UpdateWindowSurface(window)
-        iterations += 1
+        
         print("Iteration: " + str(iterations))
 
 
